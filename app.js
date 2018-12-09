0bff25754a621c7d564f71075751441f174dcb54
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-09 18:19:28 +0530

    commit 6

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ const dir = require("./util/path");
 const app = express();
 const url = path.join(dir, "views", "404.html");
 app.use(bodyParser.urlencoded({ extended: false }));
+// templete engine
+app.set("view engine", "ejs");
 // this midleware responsibale for allow access for public directory
 app.use(express.static(path.join(__dirname, "public")));
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
-app.use(adminRouter);
+app.use("/admin", adminRouter.router);
 app.use(shopRouter);
 app.use((req, res, next) => {
-  res.sendFile(url);
+  res.render("404", { title: "Page not found" });
 });
 const PORT = process.env.PORT || 8080;
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,44 @@ const path = require("path");
 const express = require("express");
 
 const router = express.Router();
+const adminData = [];
 
-const url = path.join(__dirname, "../", "views", "add-product.html");
-router.get("/admin/add-product", (req, res) => {
-  res.sendFile(url);
+// get requests
+//const url = path.join(__dirname, "../", "views", "add-product.html");
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    title: "Add Product",
+    path: "/admin/add-product"
+  });
+});
+// post request
+router.post("/admin/add-product", (req, res) => {
+  productArr.push({ title: req.body.title });
+  res.redirect("/");
 });
 
-router.get("/shop", (req, res) => {
-  res.send("Shop page ");
-});
+exports.router = router;
+exports.adminData = adminData;
 
-module.exports = router;
+// const path = require("path");
+// const express = require("express");
+
+// const router = express.Router();
+// const productArr = [];
+
+// // get requests
+// //const url = path.join(__dirname, "../", "views", "add-product.html");
+// router.get("/add-product", (req, res, next) => {
+//   res.render("add-product", {
+//     title: "Add Product",
+//     path: "/admin/add-product"
+//   });
+// });
+// // post request
+// router.post("/admin/add-product", (req, res) => {
+//   productArr.push({ title: req.body.title });
+//   res.redirect("/");
+// });
+
+// exports.router = router;
+// exports.productArr = productArr;
